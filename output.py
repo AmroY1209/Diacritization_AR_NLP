e8d054c5fe9bedbf34a809ede3757f7d4f1d82c3
AmroY1209 <amryasser1209@gmail.com>
2024-01-02 00:59:22 +0200

    fff

--- output.py
+++ output.py
@@ -1,6 +1,7 @@
 import pickle
 import torch
 from torch import nn
+import csv
 
 with open("./pickles/tashkeel_index.pkl", "rb") as file:
     tashkeel_index = pickle.load(file)
@@ -55,5 +56,38 @@ new_tensor = torch.tensor([[mapping_dict.get(elem.item(), -1) for elem in row] f
 default_value = 0
 new_tensor[new_tensor == -1] = default_value
 
-print(new_tensor[0][:10])
+print(new_tensor[0])
 
+# generate csv file
+with open('./Dataset/test/test_no_diacritics_stripped.txt', 'r', encoding='utf-8') as file:
+    test_txt = file.readlines()
+        
+list_of_sentences = []
+for sentence in test_txt:
+    list_of_sentences.append(sentence.strip())
+
+# Create a list of lists with an added ID column and a single label column
+csv_data = [['ID', 'label']]
+
+row = 0
+column = 0
+id = 0
+for sentence in list_of_sentences:
+    for char in sentence:
+        if char == " " or char == ".":
+            column += 1
+            continue
+
+        csv_data.append([id, new_tensor[row][column].item()])
+
+        id += 1
+        column += 1
+
+    row += 1
+    column = 0
+
+with open("answer.csv", mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(csv_data)
+
+print(f'CSV file has been created.')
